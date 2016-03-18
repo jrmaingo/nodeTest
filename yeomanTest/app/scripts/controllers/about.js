d643a4b0965fd0093c043134352b373563b84d8d
Julian Maingot <julian.maingot@flynncompanies.com>
2016-03-18 15:18:55 -0400

    initial commit

---
+++ yeomanTest/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coachAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the coachAppApp
+ */
+angular.module('coachAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ yeomanTest/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name coachAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the coachAppApp
+ */
+angular.module('coachAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
