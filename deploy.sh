0043503727795b69916300684820b9f9ad8c683a
Julian Maingot <julian.maingot@flynncompanies.com>
2016-04-01 14:28:02 -0400

    fixed build script to include node modules, removed prod flag from deploy

--- deploy.sh
+++ deploy.sh
@@ -112,7 +112,7 @@ selectNodeVersion
 # 3. Install npm packages
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install --production
+  eval $NPM_CMD install #--production #need to comment out to install grunt dependencies
   exitWithMessageOnError "npm failed"
   cd - > /dev/null
 else
