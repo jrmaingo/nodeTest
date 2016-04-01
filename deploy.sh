35c019fc9c025636a29dcea7f0f91dab65d2bb36
Julian Maingot <julian.maingot@flynncompanies.com>
2016-04-01 15:45:04 -0400

    changed bower package version in package.jsom, removed print from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,11 +9,11 @@
 # -------
 
 exitWithMessageOnError () {
-  if [ ! $? -eq 0 ]; then
-    echo "An error has occurred during web site deployment."
-    echo $1
-    exit 1
-  fi
+    if [ ! $? -eq 0 ]; then
+        echo "An error has occurred during web site deployment."
+        echo $1
+        exit 1
+    fi
 }
 
 # Prerequisites
@@ -32,66 +32,66 @@ ARTIFACTS=$SCRIPT_DIR/../artifacts
 KUDU_SYNC_CMD=${KUDU_SYNC_CMD//\"}
 
 if [[ ! -n "$DEPLOYMENT_SOURCE" ]]; then
-  DEPLOYMENT_SOURCE=$SCRIPT_DIR
+    DEPLOYMENT_SOURCE=$SCRIPT_DIR
 fi
 
 if [[ ! -n "$NEXT_MANIFEST_PATH" ]]; then
-  NEXT_MANIFEST_PATH=$ARTIFACTS/manifest
+    NEXT_MANIFEST_PATH=$ARTIFACTS/manifest
 
-  if [[ ! -n "$PREVIOUS_MANIFEST_PATH" ]]; then
-    PREVIOUS_MANIFEST_PATH=$NEXT_MANIFEST_PATH
-  fi
+    if [[ ! -n "$PREVIOUS_MANIFEST_PATH" ]]; then
+        PREVIOUS_MANIFEST_PATH=$NEXT_MANIFEST_PATH
+    fi
 fi
 
 if [[ ! -n "$DEPLOYMENT_TARGET" ]]; then
-  DEPLOYMENT_TARGET=$ARTIFACTS/wwwroot
+    DEPLOYMENT_TARGET=$ARTIFACTS/wwwroot
 else
-  KUDU_SERVICE=true
+    KUDU_SERVICE=true
 fi
 
 if [[ ! -n "$KUDU_SYNC_CMD" ]]; then
-  # Install kudu sync
-  echo Installing Kudu Sync
-  npm install kudusync -g --silent
-  exitWithMessageOnError "npm failed"
+    # Install kudu sync
+    echo Installing Kudu Sync
+    npm install kudusync -g --silent
+    exitWithMessageOnError "npm failed"
 
-  if [[ ! -n "$KUDU_SERVICE" ]]; then
-    # In case we are running locally this is the correct location of kuduSync
-    KUDU_SYNC_CMD=kuduSync
-  else
-    # In case we are running on kudu service this is the correct location of kuduSync
-    KUDU_SYNC_CMD=$APPDATA/npm/node_modules/kuduSync/bin/kuduSync
-  fi
+    if [[ ! -n "$KUDU_SERVICE" ]]; then
+        # In case we are running locally this is the correct location of kuduSync
+        KUDU_SYNC_CMD=kuduSync
+    else
+        # In case we are running on kudu service this is the correct location of kuduSync
+        KUDU_SYNC_CMD=$APPDATA/npm/node_modules/kuduSync/bin/kuduSync
+    fi
 fi
 
 # Node Helpers
 # ------------
 
 selectNodeVersion () {
-  if [[ -n "$KUDU_SELECT_NODE_VERSION_CMD" ]]; then
-    SELECT_NODE_VERSION="$KUDU_SELECT_NODE_VERSION_CMD \"$DEPLOYMENT_SOURCE\" \"$DEPLOYMENT_TARGET\" \"$DEPLOYMENT_TEMP\""
-    eval $SELECT_NODE_VERSION
-    exitWithMessageOnError "select node version failed"
+    if [[ -n "$KUDU_SELECT_NODE_VERSION_CMD" ]]; then
+        SELECT_NODE_VERSION="$KUDU_SELECT_NODE_VERSION_CMD \"$DEPLOYMENT_SOURCE\" \"$DEPLOYMENT_TARGET\" \"$DEPLOYMENT_TEMP\""
+        eval $SELECT_NODE_VERSION
+        exitWithMessageOnError "select node version failed"
 
-    if [[ -e "$DEPLOYMENT_TEMP/__nodeVersion.tmp" ]]; then
-      NODE_EXE=`cat "$DEPLOYMENT_TEMP/__nodeVersion.tmp"`
-      exitWithMessageOnError "getting node version failed"
+        if [[ -e "$DEPLOYMENT_TEMP/__nodeVersion.tmp" ]]; then
+            NODE_EXE=`cat "$DEPLOYMENT_TEMP/__nodeVersion.tmp"`
+            exitWithMessageOnError "getting node version failed"
+        fi
+
+        if [[ -e "$DEPLOYMENT_TEMP/__npmVersion.tmp" ]]; then
+            NPM_JS_PATH=`cat "$DEPLOYMENT_TEMP/__npmVersion.tmp"`
+            exitWithMessageOnError "getting npm version failed"
+        fi
+
+        if [[ ! -n "$NODE_EXE" ]]; then
+            NODE_EXE=node
+        fi
+
+        NPM_CMD="\"$NODE_EXE\" \"$NPM_JS_PATH\""
+    else
+        NPM_CMD=npm
+        NODE_EXE=node
     fi
-
-    if [[ -e "$DEPLOYMENT_TEMP/__npmVersion.tmp" ]]; then
-      NPM_JS_PATH=`cat "$DEPLOYMENT_TEMP/__npmVersion.tmp"`
-      exitWithMessageOnError "getting npm version failed"
-    fi
-
-    if [[ ! -n "$NODE_EXE" ]]; then
-      NODE_EXE=node
-    fi
-
-    NPM_CMD="\"$NODE_EXE\" \"$NPM_JS_PATH\""
-  else
-    NPM_CMD=npm
-    NODE_EXE=node
-  fi
 }
 
 ##################################################################################################################################
@@ -111,39 +111,39 @@ selectNodeVersion
 
 # 3. Install npm packages
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install #--production #need to comment out to install grunt dependencies
-  exitWithMessageOnError "npm failed"
-  cd - > /dev/null
+    cd "$DEPLOYMENT_TARGET"
+    eval $NPM_CMD install #--production #need to comment out to install grunt dependencies
+    exitWithMessageOnError "npm failed"
+    cd - > /dev/null
 else
-  echo "package.json not found"
+    echo "package.json not found"
 fi
 
 # 4. Install bower
 if [ -e "$DEPLOYMENT_TARGET/bower.json" ]; then
-  cd "$DEPLOYMENT_TARGET"
-  echo skipping bower actions
-  eval $NPM_CMD install bower
-  exitWithMessageOnError "installing bower failed"
-   echo cleaning bower cache
-   eval ./node_modules/.bin/bower cache clean
-  eval ./node_modules/.bin/bower install
-  exitWithMessageOnError "bower install failed"
-  cd - > /dev/null
+    cd "$DEPLOYMENT_TARGET"
+    echo starting bower actions
+    eval $NPM_CMD install bower
+    exitWithMessageOnError "installing bower failed"
+    echo cleaning bower cache
+    eval ./node_modules/.bin/bower cache clean
+    eval ./node_modules/.bin/bower install
+    exitWithMessageOnError "bower install failed"
+    cd - > /dev/null
 else
-  echo "bower.json not found"
+    echo "bower.json not found"
 fi
 
 # 5. Install grunt
 if [ -e "$DEPLOYMENT_TARGET/Gruntfile.js" ]; then
-  cd "$DEPLOYMENT_TARGET"
-#  eval $NPM_CMD install grunt-cli
-#  exitWithMessageOnError "installing grunt failed"
-  eval ./node_modules/.bin/grunt --no-color build
-  exitWithMessageOnError "grunt build failed"
-  cd - > /dev/null
+    cd "$DEPLOYMENT_TARGET"
+    #  eval $NPM_CMD install grunt-cli
+    #  exitWithMessageOnError "installing grunt failed"
+    eval ./node_modules/.bin/grunt --no-color build
+    exitWithMessageOnError "grunt build failed"
+    cd - > /dev/null
 else
-  echo "Gruntfile.js not found"
+    echo "Gruntfile.js not found"
 fi
 
 # 6. KuduSync Again?
@@ -154,10 +154,10 @@ exitWithMessageOnError "Kudu Sync 2 failed"
 
 # Post deployment stub
 if [[ -n "$POST_DEPLOYMENT_ACTION" ]]; then
-  POST_DEPLOYMENT_ACTION=${POST_DEPLOYMENT_ACTION//\"}
-  cd "${POST_DEPLOYMENT_ACTION_DIR%\\*}"
-  "$POST_DEPLOYMENT_ACTION"
-  exitWithMessageOnError "post deployment action failed"
+    POST_DEPLOYMENT_ACTION=${POST_DEPLOYMENT_ACTION//\"}
+    cd "${POST_DEPLOYMENT_ACTION_DIR%\\*}"
+    "$POST_DEPLOYMENT_ACTION"
+    exitWithMessageOnError "post deployment action failed"
 fi
 
 echo "Finished successfully."
